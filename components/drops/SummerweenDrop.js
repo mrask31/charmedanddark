a7a160b1d79926753947f517d7dbe426d6a396c3
mrask31 <admin@mjrintelligencegroup.com>
2026-05-29 14:38:07 -0500

    feat: add images to all Summerween product tiles

--- components/drops/SummerweenDrop.js
+++ components/drops/SummerweenDrop.js
@@ -4,11 +4,11 @@ import Link from 'next/link';
 
 const SUMMERWEEN_PRODUCTS = [
   { name: 'Summerween Women\'s Flowy Scoop Muscle Tank', handle: 'summerween-womens-flowy-scoop-muscle-tank-1', image: 'https://cdn.shopify.com/s/files/1/0861/2079/2098/files/20260508131823-1f14ae06-3bbf-61ee-9838-cafecbf14569.png?v=1778246475' },
-  { name: 'Summerween Trucker Snapback Hat', handle: 'summerween-trucker-snapback-hat' },
-  { name: 'Bones and Brew Summer Unisex Tee', handle: 'bones-and-brew-summer-unisex-tee-1' },
-  { name: 'Hexes & Heat Unisex Summer Tee', handle: 'hexes-heat-unisex-summer-tee-1' },
-  { name: 'Salty Spells & Sunset Sins Women\'s Boxy Tee', handle: 'salty-spells-sunset-sins-womens-boxy-tee-1' },
-  { name: 'Camp Charmed and Dark Unisex Ringer Tee', handle: 'camp-charmed-and-dark-unisex-ringer-tee' },
+  { name: 'Summerween Trucker Snapback Hat', handle: 'summerween-trucker-snapback-hat', image: 'https://cdn.shopify.com/s/files/1/0861/2079/2098/files/20260508122159-1f14ad88-2c8c-6c2c-8bc9-cafecbf14569.png?v=1778243048' },
+  { name: 'Bones and Brew Summer Unisex Tee', handle: 'bones-and-brew-summer-unisex-tee-1', image: 'https://cdn.shopify.com/s/files/1/0861/2079/2098/files/20260508135620-1f14ae5b-157b-6384-8c7e-6263bc339f57.png?v=1778248645' },
+  { name: 'Hexes & Heat Unisex Summer Tee', handle: 'hexes-heat-unisex-summer-tee-1', image: 'https://cdn.shopify.com/s/files/1/0861/2079/2098/files/20260508134651-1f14ae45-e112-686e-a3c7-cafecbf14569.png?v=1778248310' },
+  { name: 'Salty Spells & Sunset Sins Women\'s Boxy Tee', handle: 'salty-spells-sunset-sins-womens-boxy-tee-1', image: 'https://cdn.shopify.com/s/files/1/0861/2079/2098/files/20260508130747-1f14adee-8a0a-6686-88f9-ba627eb4d203.png?v=1778245858' },
+  { name: 'Camp Charmed and Dark Unisex Ringer Tee', handle: 'camp-charmed-and-dark-unisex-ringer-tee', image: 'https://cdn.shopify.com/s/files/1/0861/2079/2098/files/20260508141331-1f14ae81-772e-6b64-99a4-4e55412b67c7.png?v=1778249803' },
 ];
 
 export default function SummerweenDrop() {
@@ -52,6 +52,8 @@ export default function SummerweenDrop() {
               <img
                 src={product.image}
                 alt={product.name}
+                loading="lazy"
+                decoding="async"
                 className="h-16 w-16 object-cover shrink-0"
                 style={{ backgroundColor: '#08080f' }}
               />
